cdbfbdf57dd33e039acd9c2da5a3d67a0348ecc0
nitinkumarmittal <nitinmittal243@gmail.com>
2020-12-05 16:49:46 -0500

    Further text cleaning and topic modelling

--- src/modern_slavery_registry/utils.py
+++ src/modern_slavery_registry/utils.py
@@ -1,21 +1,35 @@
 import pandas as pd
-import multiprocessing.pool
+# import multiprocessing.pool
 import functools
+from hashlib import md5
+from typing import Dict
 
 def fix_unicode(df: pd.core.frame.DataFrame):
     return df.applymap(lambda x: x.encode('unicode_escape').decode('utf-8') if isinstance(x, str) else x)
 
-# https://stackoverflow.com/a/35139284
-def timeout(max_timeout):
-    """Timeout decorator, parameter in seconds."""
-    def timeout_decorator(item):
-        """Wrap the original function."""
-        @functools.wraps(item)
-        def func_wrapper(*args, **kwargs):
-            """Closure for function."""
-            pool = multiprocessing.pool.ThreadPool(processes=1)
-            async_result = pool.apply_async(item, args, kwargs)
-            # raises a TimeoutError if execution exceeds max_timeout
-            return async_result.get(max_timeout)
-        return func_wrapper
-    return timeout_decorator
+# # https://stackoverflow.com/a/35139284
+# def timeout(max_timeout):
+#     """Timeout decorator, parameter in seconds."""
+#     def timeout_decorator(item):
+#         """Wrap the original function."""
+#         @functools.wraps(item)
+#         def func_wrapper(*args, **kwargs):
+#             """Closure for function."""
+#             pool = multiprocessing.pool.ThreadPool(processes=1)
+#             async_result = pool.apply_async(item, args, kwargs)
+#             # raises a TimeoutError if execution exceeds max_timeout
+#             return async_result.get(max_timeout)
+#         return func_wrapper
+#     return timeout_decorator
+
+def generate_hash(string: str) -> str:
+    """Generate hash for input string"""
+    return  md5(string=f"{string}".encode()).hexdigest()
+
+def sort_dict(dict_: Dict,
+              by: int= 0, 
+              reverse: bool=False):
+    """Sort and return input dictionary"""
+    return {k:v for k,v in sorted(dict_.items(), 
+                                  key=lambda x:x[by], 
+                                  reverse=reverse)}
