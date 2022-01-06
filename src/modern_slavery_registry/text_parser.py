44f3f5eae752ff258e99dfa47b0e4c49cb2e71dd
nitinkmittal <nitinmittal243@gmail.com>
2022-01-06 13:23:17 -0500

    Added recent changes

--- src/modern_slavery_registry/text_parser.py
+++ src/modern_slavery_registry/text_parser.py
@@ -96,9 +96,7 @@ def find_urls_in_text(text: str) -> List:
     # findall() has been used
     # with valid conditions for urls in string
     regex = (
-        r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)"
-        "(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()"
-        "<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+        r"\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))"
     )
     url = re.findall(regex, text)
 
